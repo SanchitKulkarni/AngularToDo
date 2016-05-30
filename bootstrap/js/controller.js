be52f674905c1713e83d53e421270e8eb7f7e376
SanchitKulkarni <sanchitkulkarni3@gmail.com>
2016-05-30 19:55:47 +0530

    second commit

--- bootstrap/js/controller.js
+++ bootstrap/js/controller.js
@@ -1,16 +1,15 @@
 var app = angular.module("ToDo",[]);
 app.controller("today",function($scope){
 	$scope.list = ["Make a To-Do App","Call Raghav for doubts"];
-});
-app.controller("tommorow",function($scope){
-	$scope.list = ["Make a To-Do App Tomorrow","Call Raghav tomorrow too"];
 	$scope.func = function(){
 		if($scope.input==true)
 			$scope.input=false;
 		else
 			$scope.input=true;
-		alert("hi");
 	}
+});
+app.controller("tommorow",function($scope){
+	$scope.list = ["Make a To-Do App Tomorrow","Call Raghav tomorrow too"];
 });
 app.controller("week",function($scope){
 	$scope.list = ["Make a To-Do App next week","Call Raghav next week"];
